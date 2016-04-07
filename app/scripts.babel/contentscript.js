8707c17bbd1f35e7fd0206dca508851f861a08f2
Joe Wolff <wolffj@google.com>
2016-04-07 13:23:28 -0700

    now we are cooking with gas

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -2,7 +2,7 @@
 
 console.log('\'Allo \'Allo! Content script');
 
-var doc = document;
+/*var doc = document;
 var sidebarInner = doc.querySelector('.sidebar-inner');
 var sidebarContents = sidebarInner.children;
 var widget = '<div class="foobar>I AM FULL OF TEST MEAT</div>'
@@ -11,6 +11,42 @@ console.log('whats up doc', doc, sidebarInner);
 console.log('first child', sidebarInner.firstChild);
 console.log('contents', sidebarContents);
 
-sidebarContents[1].insertAdjacentHTML('beforebegin', widget);
+sidebarContents[1].insertAdjacentHTML('beforebegin', widget);*/
 
-//sidebarInner.insertBefore(widget, sidebarInner.firstChild);
+//sidebarInner.insertBefore(widget, sidebarInner.firstChild); 
+
+window.addEventListener('load', function() {
+  var app = angular.module('amz', []);
+  var html = document.querySelector('html');
+  var sidebarInner = document.querySelector('.sidebar-inner');
+  var feedDirective = document.createElement('div');
+
+  html.setAttribute('ng-app', '');
+  html.setAttribute('ng-csp', '');
+
+  sidebarInner.setAttribute('ng-controller', 'MainController');
+
+  angular
+    .module('amz')
+    .controller('MainController', function($scope) {
+      console.log('yo controlling');
+    });
+
+  console.log('so far, right?');
+
+  feedDirective.setAttribute('feed-directive', '');
+  feedDirective.setAttribute('class', 'amz-feed');
+
+  sidebarInner.appendChild(feedDirective);
+
+  angular
+    .module('amz')
+    .directive('feedDirective', function() {
+      return {
+        restrict: 'EA',
+        template: '<input value="Search on Bing" name="btnB" type="submit">'
+      };
+    });
+
+  angular.bootstrap(html, ['amz'], []);
+});
