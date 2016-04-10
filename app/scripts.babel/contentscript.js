38fb57d5016626be7a8fb68ba85139d7b3c36a4b
Joe Wolff <wolffj@google.com>
2016-04-10 15:29:19 -0700

    added ng-carousel

--- app/scripts.babel/contentscript.js
+++ app/scripts.babel/contentscript.js
@@ -2,21 +2,8 @@
 
 console.log('\'Allo \'Allo! Content script');
 
-/*var doc = document;
-var sidebarInner = doc.querySelector('.sidebar-inner');
-var sidebarContents = sidebarInner.children;
-var widget = '<div class="foobar>I AM FULL OF TEST MEAT</div>'
-
-console.log('whats up doc', doc, sidebarInner);
-console.log('first child', sidebarInner.firstChild);
-console.log('contents', sidebarContents);
-
-sidebarContents[1].insertAdjacentHTML('beforebegin', widget);*/
-
-//sidebarInner.insertBefore(widget, sidebarInner.firstChild); 
-
 window.addEventListener('load', function() {
-  var app = angular.module('amz', []);
+  var app = angular.module('amz', ['ngAnimate']);
   var html = document.querySelector('html');
   var sidebarInner = document.querySelector('#sidebar');
   var feedDirective = document.createElement('div');
